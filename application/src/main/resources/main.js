0dccfa310d8676d3350a359b4e4091e74d9236e3
Nikita Marshalkin <marnikitta@gmail.com>
2020-12-19 16:37:24 +0300

    Move caret to the end of a textarea

--- application/src/main/resources/main.js
+++ application/src/main/resources/main.js
@@ -1,3 +1,8 @@
+window.onload = function() {
+    autoGrowTextField(document.getElementById('discussion-edit'));
+    return false;
+  };
+
 function autoGrowTextField(element) {
     element.style.height = "5px"
     element.style.height = (element.scrollHeight + 27 * 2) + "px";
@@ -16,6 +21,7 @@ function addOutlink(outlinkName) {
     discussionEdit.value = discussionValue
 
     autoGrowTextField(discussionEdit)
+    discussionEdit.setSelectionRange(discussionEdit.value.length,discussionEdit.value.length);
     discussionEdit.focus()
     return false;
 }

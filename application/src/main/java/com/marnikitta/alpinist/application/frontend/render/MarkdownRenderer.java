c226b30b95ae48c46cc000012217aa7d11f3384e
Nikita Marshalkin <marnikitta@gmail.com>
2020-12-27 12:29:17 +0300

    Fix race

--- application/src/main/java/com/marnikitta/alpinist/application/frontend/render/MarkdownRenderer.java
+++ application/src/main/java/com/marnikitta/alpinist/application/frontend/render/MarkdownRenderer.java
@@ -5,15 +5,15 @@ import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.parser.Parser;
 import com.vladsch.flexmark.util.options.MutableDataSet;
 
-import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class MarkdownRenderer {
   private static final MutableDataSet options = new MutableDataSet();
   private static final Parser parser = Parser.builder(options).build();
   private static final HtmlRenderer renderer = HtmlRenderer.builder(options).build();
 
-  private final Map<String, String> renderingCache = new LinkedHashMap<>();
+  private final Map<String, String> renderingCache = new ConcurrentHashMap<>();
 
   public String renderMarkdown(String md) {
     while (renderingCache.size() > 10000) {
